87024db165c07622e4109726d77061a18695c5b9
Amandine <amandine.labaeye@orange.fr>
2018-11-23 10:33:24 +0100

    commit

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,25 @@
 // Make it count up on a timer, calling this function
 var seconds = document.getElementById("seconds");
+var time = 0;
+var count;
 var countUp = function() {
-    // ?
+
+    seconds.innerHTML = time++;
+    console.log(seconds);
+    count = setTimeout(countUp, 1000);
+
 };
+
+countUp();
+
 
 // How can you make it stop counting?
 var stopCountUp = function() {
+
+    clearTimeout(count);
+
 };
+
 var stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
 
